53dce79a17efac6e74b3b1b5da856c1095faa303
siddharthbahl <bahlsiddharth123@gmail.com>
2023-03-20 02:31:18 +0000

    commit

--- backend.py
+++ backend.py
@@ -30,7 +30,7 @@ def setArray(temperature, place):
 @app.route("/")
 def index():
     temperature, precipitation, humidity, wind_speed = get_weather("London")
-    if temperature < 9:
+    if int(temperature) < 9:
          image = "cold.png"
     else:
          image = "hot.png"
